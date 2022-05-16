d479a4df78be75c4b29c65b6335b3a4118f52501
Hang Nguyen <thuhang.nguyen@relexsolutions.com>
2022-05-16 14:35:44 +0300

    pipe and finish exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TeamComponent } from './team/team.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeamComponent
+    TeamComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
